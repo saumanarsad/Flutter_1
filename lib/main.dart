5b207b430d16de9acc4f88850dc8a0e6815d419d
saumanarsad <sauman.arshad@gmail.com>
2024-07-21 03:27:51 +0500

    Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -33,23 +33,18 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('My flutter'),
-      ),
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 100,
-          color: Colors.grey,
-          child: const Center(
-            child: Text(
-              'Center of container',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('My flutter'),
         ),
-      ),
-    );
+        body: OutlinedButton(
+          child: const Text('Hello'),
+          onPressed: () {
+            print('hello');
+          },
+          onLongPress: () {
+            print("long press");
+          },
+        ));
   }
 }
